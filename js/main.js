0ea1ffe288ab0d00c41fcb65f364ac52de9edb8f
Heinrich Fenkart <hnrch02@gmail.com>
2014-06-25 22:12:40 +0200

    Small rework of the JS

--- js/main.js
+++ js/main.js
@@ -1,61 +1,65 @@
-(function() { "use strict";
+(function() {
+  'use strict';
 
-  if(!document.querySelector || !document.querySelectorAll || !window.addEventListener || !document.body.classList || !window.postMessage)
-    return alert('Um Himmels Willen, update doch mal deinen Browser!');
+  var Spy = function() {
+    this.update();
+  };
 
-  if(document.querySelector('.js-spy')) {
-    var spy = {
+  Spy.prototype.update = function() {
+    this.elements  = document.querySelector('.js-spy');
+    this.selectors = this.elements.querySelectorAll('[href^="#"]');
+    this.targets   = [];
+    this.offsets   = [];
+    this.current;
 
-      update: function() {
-        this.elements  = document.querySelector('.js-spy');
-        this.selectors = this.elements.querySelectorAll('[href^="#"]');
-        this.targets   = [];
-        this.offsets   = [];
-        this.current;
-
-        for(var i = this.selectors.length - 1; i >= 0; i--) {
-          this.targets[i] = document.querySelector(this.selectors[i].hash);
-          this.offsets[i] = this.targets[i].offsetTop;
-        };
-
-        this.process();
-      },
-
-      process: function() {
-        var old = this.current;
-
-        for(var i = 0; i <= this.offsets.length - 1; i++) {
-          if(window.scrollY >= this.offsets[i])
-            this.current = this.selectors[i];
-        };
-
-        if((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-          this.current = this.selectors[this.selectors.length - 1];
-        }
-
-        if(this.current !== old) {
-          if(old) old.parentNode.classList.remove('active');
-          this.current.parentNode.classList.add('active');
-        }
-      }
-
+    for(var i = this.selectors.length - 1; i >= 0; i--) {
+      this.targets[i] = document.querySelector(this.selectors[i].hash);
+      this.offsets[i] = this.targets[i].offsetTop;
     };
 
-    window.addEventListener('load', function() {
-      spy.update();
+    this.process();
+  };
 
-      window.addEventListener('resize', function() { spy.update(); }, false);
-      window.addEventListener('scroll', function() { spy.process(); }, false);
+  Spy.prototype.process = function() {
+    var old = this.current;
+
+    for(var i = 0; i <= this.offsets.length - 1; i++) {
+      if(window.scrollY >= this.offsets[i]) this.current = this.selectors[i];
+    };
+
+    if((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+      this.current = this.selectors[this.selectors.length - 1];
+    }
+
+    if(this.current !== old) {
+      if(old) old.parentNode.classList.remove('active');
+      this.current.parentNode.classList.add('active');
+    }
+  }
+
+  if(!document.querySelector ||
+     !document.querySelectorAll ||
+     !document.documentElement.classList ||
+     !window.addEventListener ||
+     !window.postMessage ||
+     !Function.prototype.bind) {
+    return alert('Um Himmels Willen, update doch mal deinen Browser!');
+  }
+
+  if(document.querySelector('.js-spy')) {
+    window.addEventListener('load', function() {
+      var spy = new Spy();
+
+      window.addEventListener('resize', spy.update.bind(spy), false);
+      window.addEventListener('scroll', spy.process.bind(spy), false);
     }, false);
   }
 
   if(window.parent !== window) {
     document.addEventListener('keydown', function(e) {
-      if(e.keyCode !== 27) {
-        return;
-      }
+      if(e.keyCode !== 27) return;
 
       window.parent.postMessage('keydown:27', '*');
-    });
+    }, false);
   }
 })();
